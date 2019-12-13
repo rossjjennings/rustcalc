5d64fbd6c1507c330a5f1831d3b6bc48541bd186
Ross Jennings <rossjjennings@gmail.com>
2019-12-12 23:47:45 -0500

    Fail more gracefully when no argument is provided

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use std::env;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    let x = rustcalc::parse(&args[1]);
+    
+    let arg = args.get(1);
+    if arg == None {
+        println!("Please enter an expression!");
+        return;
+    }
+    let arg = arg.unwrap();
+    
+    let x = rustcalc::parse(arg);
     match x {
     | Ok(expr) => println!("{}", expr.eval()), 
     | Err(e) => println!("Error parsing input: {}", e),
